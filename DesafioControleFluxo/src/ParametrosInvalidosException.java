45d3732202a877e850d7c929438ff1d44c0c8f17
Higor Robin <proghigorrobin@gmail.com>
2025-02-19 21:29:41 -0300

    Controle de Fluxo - Desafio

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+/**
+ * @author higor.robinn on 19/02/2025.
+ */
+
+public class ParametrosInvalidosException extends Exception{
+
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
